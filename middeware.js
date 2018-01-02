dbbe406d1ee94000dd7d040bfed7231063651875
Rutabadev <etienner37@gmail.com>
2018-01-02 11:21:32 +0100

    startup fix and jshint conf

--- middeware.js
+++ middeware.js
@@ -1,9 +1,7 @@
-'use strict';
-
 module.exports = function (req, res) {
-
+    'use strict';
     // Error 404 handling
     res.status(404).send({
         url: req.originalUrl + ' not found'
-    })
+    });
 };
